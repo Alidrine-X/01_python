b469305d4942d6814361927a36e57ed8b9a6a3ab
Alidrine-X <lenawide@gmail.com>
2026-01-13 19:55:52 +0100

    Commit efter övning 2

--- p1.py
+++ p1.py
@@ -1,6 +1,13 @@
-# veckouppgift 1
+# veckouppgift 1-1
 
+# När man kör filen ska programmet skriva ut följande på terminalen,
+# fast byt ut "Ditt Namn" mot ditt namn:
+# "Hello world"
+# "This program was made by Ditt Namn"
+
+# ----------------------------------------------------------------
 # Hälsa världen välkommen och presentera vem som skapat koden
 developer = "Lena"
 print("Hello World")
 print("This program was created by " + developer)
+# ----------------------------------------------------------------
---
+++ p2.py
@@ -0,0 +1,25 @@
+# Veckouppgift 1-2
+# Denna kod ska räkna ut hur mycket som blir kvar, efter att man betalat en biljett.
+# Pengarna som blir över ska du dela med en vän.
+# Men koden är inte testad. Finns det några fel i koden?
+
+# x = 100  # biljettpris
+# y = 200  # pengar på fickan
+# print("Det blir " + (y - x) " kronor över.")
+# z = y - x / 2
+# print("Varje person får " + z)
+
+# ----------------------------------------------------------------
+# Försök rätta fel, så att programmet kan ge rätt svar.
+# Hitta på bättre namn, i stället för x, y och z.
+
+# Räkna ut hur mycket pengar vardera person får efter att biljetten är betalad
+price = 100
+wallet = 200
+print("Det blir " + str(wallet - price) + " kronor över.")
+print("Varje person får " + str(int((wallet - price)/2)) + " kronor.")
+# ----------------------------------------------------------------
+
+
+
+
