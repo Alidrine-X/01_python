a765b744cfb47442c56e6dd29d9bc82509d3c7ce
Alidrine-X <lenawide@gmail.com>
2026-01-13 21:17:39 +0100

    Commit efter övning 3

---
+++ p31.py
@@ -0,0 +1,32 @@
+# Veckouppgift 1-3-1
+
+# 3 Använda variabler och datatyper
+# ----------------------------------------------------------------
+
+# 1a Använd input för att be användaren om ett heltal.
+# Spara värdet i en variabel.
+# Omvandla variabelns värde till ett heltal, och skriv ut det för att testa om du har gjort rätt.
+# Kodexempel med input:
+# x = input("Fråga här")
+#
+# 1b Fråga användaren efter ett annat heltal.
+# Skriv ut summan av talen, alltså tal1 + tal2.
+# Testa genom att hitta på två tal och räkna ut summan i huvudet.
+# Kontrollera om programmet räknar rätt.
+#
+# ----------------------------------------------------------------
+# 1a
+print("Uppgift 1-3-1a")
+integer_1 = input("Ange ett heltal: ")
+print("Du skrev talet: " + str(integer_1))
+print("----------------------------------------------------------------")
+
+# ----------------------------------------------------------------
+# 1b
+print("Uppgift 1-3-1b")
+integer_1 = int(input("Ange ett heltal: "))
+integer_2 = int(input("Ange ett heltal till: "))
+print("Summan av talen är: " + str(integer_1 + integer_2))
+print("----------------------------------------------------------------")
+
+# ----------------------------------------------------------------
---
+++ p41.py
@@ -0,0 +1,37 @@
+# Veckouppgift 1-4-1
+
+# 4 Fler övningar
+# ----------------------------------------------------------------
+
+# Lite mer avancerad nivå.
+#
+# 1a Det är ca 470 km mellan Stockholm och Göteborg. Skriv ett program som räknar ut hur lång tid det tar att köra från Stockholm till Göteborg. Du behöver fråga användaren hur fort man ska köra, i km/h. Svara i timmar.
+#
+# 1b Gör så att programmet svarar i minuter i stället för timmar.
+#
+# 1c (svårare) Nu ska programmet svara i hela timmar och minuter.
+# Tips: använd operatorerna // och %. Heltalsdivision // dividerar och avrundar nedåt till närmaste heltal. Procent % räknar ut resten vid division med heltal. Exempel:
+# 3 // 2 == 1      (3 / 2 == 1.5, avrundar nedåt)
+# 60 % 60 == 0  (ingen rest)
+# 70 % 60 == 10  (10 i rest)
+# Be en AI förklara heltalsdivision och modulo i Python om du känner dig osäker!
+
+# ----------------------------------------------------------------
+# 4a
+print("Uppgift 1-4-1a")
+
+print("----------------------------------------------------------------")
+
+# ----------------------------------------------------------------
+# 4b
+print("Uppgift 1-4-1b")
+
+print("----------------------------------------------------------------")
+
+# ----------------------------------------------------------------
+# 4c
+print("Uppgift 1-4-1c")
+
+print("----------------------------------------------------------------")
+
+# ----------------------------------------------------------------
